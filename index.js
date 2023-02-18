8ef551c69aa533b880d693b252de4af8920b9d84
Albert Akrong <alphaxsalt@gmail.com>
2023-02-18 17:41:19 +0000

    buynow
    add buy now function

--- index.js
+++ index.js
@@ -5,3 +5,14 @@
 // review.addEventListener("click", function(){
 //     buyFlex.innerHTML = "I made it";
 // })
+
+function buyNow(itemDetails) {
+  const itemsInCart = JSON.parse?.(localStorage.getItem("itemsInCart")) || [];
+  itemsInCart.push(itemDetails);
+  localStorage.setItem("itemsInCart", JSON.stringify(itemsInCart));
+}
+
+function viewCart() {
+  const itemsInCart = JSON.parse?.(localStorage.getItem("itemsInCart")) || [];
+  console.log(itemsInCart);
+}
